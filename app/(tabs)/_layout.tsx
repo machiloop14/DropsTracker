d2ddce89990f5b5c8e02346fee25eb68d76429c9
machiloop <hamidusodiq2@gmail.com>
2026-01-17 10:14:00 +0100

    feat(ui): desgin dashboard screen dark theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,6 +16,7 @@ const TabsLayout = () => {
         headerShown: false,
         tabBarStyle: {
           backgroundColor: tabBg,
+          borderColor: "transparent",
         },
       }}
     >
--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -2,6 +2,7 @@ import ThemeToggler from "@/components/themeToggler";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
 import { AnimatedCircularProgress } from "react-native-circular-progress";
@@ -9,35 +10,36 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Dashboard = () => {
   const insets = useSafeAreaInsets();
+  const { colorScheme } = useColorScheme();
 
   return (
     <View
       style={{ paddingTop: insets.top }}
-      className="bg-[#f2f3fe] flex-1 px-5"
+      className="bg-[#f2f3fe] dark:bg-[#0b1020] flex-1 px-5"
     >
       {/* header */}
       <View className="flex flex-row justify-between items-center pt-8 pb-2">
         <View className="flex-row gap-2 items-center">
-          <View className="bg-[#06b6d4] px-[5px] py-[5px] rounded-md">
+          <View className="bg-[#06b6d4] dark:bg-[#00ffd1] px-[5px] py-[5px] rounded-md">
             <MaterialCommunityIcons
               name="cube-outline"
               size={18}
-              color="white"
+              color={colorScheme == "dark" ? "black" : "white"}
             />
           </View>
-          <Text className="color-[#0f1721] font-spaceBold text-2xl">
+          <Text className="text-[#0f1721] dark:text-white font-spaceBold text-2xl">
             AirTrack
           </Text>
         </View>
         <View className="flex-row gap-2 items-center">
-          <View className="bg-white px-[5px] py-[5px] rounded-md">
+          <View className="bg-white dark:bg-[#1e293b] px-[5px] py-[5px] rounded-md">
             <MaterialCommunityIcons
               name="filter-outline"
-              size={20}
-              color="#6b7280"
+              size={22}
+              color={colorScheme == "dark" ? "#f8fafc" : "#6b7280"}
             />
           </View>
-          <View className="bg-white px-[5px] py-[5px] rounded-md">
+          <View className="bg-white dark:bg-[#1e293b] px-[5px] py-[5px] rounded-md">
             {/* <MaterialCommunityIcons
               name="bell-outline"
               size={20}
@@ -49,9 +51,9 @@ const Dashboard = () => {
       </View>
       {/* summary section */}
       <View className="flex-row justify-between w-full  pt-2">
-        <View className="bg-white w-[47%] py-4 px-3 rounded-lg gap-4">
+        <View className="bg-white dark:bg-[#0f1726] w-[47%] py-4 px-3 rounded-lg gap-4">
           <View className="flex-row items-center justify-between">
-            <Text className="font-spaceRegular text-sm  text-[#6b7280]">
+            <Text className="font-spaceRegular text-sm  text-[#6b7280] dark:text-[#e6e9ff]">
               Tasks due
             </Text>
             <MaterialCommunityIcons
@@ -60,38 +62,45 @@ const Dashboard = () => {
               size={14}
             />
           </View>
-          <Text className="text-[#06b6d4] font-spaceBold text-2xl">12</Text>
+          <Text className="text-[#06b6d4] dark:text-[#00ffd1] font-spaceBold text-2xl">
+            12
+          </Text>
         </View>
-        <View className="bg-white w-[47%] py-4 px-3 rounded-lg gap-4">
+        <View className="bg-white dark:bg-[#0f1726] w-[47%] py-4 px-3 rounded-lg gap-4">
           <View className="flex-row items-center justify-between">
-            <Text className="font-spaceRegular text-sm  text-[#6b7280]">
+            <Text className="font-spaceRegular text-sm  text-[#6b7280] dark:text-[#e6e9ff]">
               Total drops
             </Text>
             <Ionicons name="sparkles-outline" color="#8b5cf6" size={14} />
           </View>
-          <Text className="text-[#06b6d4] font-spaceBold text-2xl">24</Text>
+          <Text className="text-[#06b6d4] dark:text-[#00ffd1] font-spaceBold text-2xl">
+            24
+          </Text>
         </View>
       </View>
 
       {/* Drops List */}
       <View className="flex-1">
         <View className="flex-row justify-between items-center pt-6">
-          <Text className="text-base text-[#020617] font-spaceBold">
+          <Text className="text-base text-[#020617] dark:text-[#e6f0ff] font-spaceBold">
             Active Drops
           </Text>
-          <Text className="text-sm text-[#06b6d4] font-spaceMedium">
+          <Text className="text-sm text-[#06b6d4] dark:text-[#00ffd1] font-spaceMedium">
             View all
           </Text>
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <View className="mt-4 bg-white border-l-2 border-[#f59e0b] px-2.5 rounded-xl">
+          <View className="mt-4 bg-white dark:bg-[#0f1726] border-l-2 border-[#f59e0b] px-2.5 rounded-xl">
             <View className="flex-row justify-between items-center py-4">
               <View>
-                <Text className="font-spaceBold text-base text-[#020617]">
+                <Text className="font-spaceBold text-base text-[#020617] dark:text-white">
                   zkSync Era
                 </Text>
-                <View className="bg-[#06b6d414] p-1 rounded-md font-spaceSemibold">
-                  <Text className="font-spaceRegular text-xs text-[#06b6d4] uppercase">
+                <View className="bg-[#06b6d414] dark:bg-[##102b3a] p-1 rounded-md font-spaceSemibold">
+                  <Text
+                    className="font-spaceRegular text-xs text-[#06b6d4]
+                  dark:text-[#00ffd1] uppercase"
+                  >
                     Bridge & Swap
                   </Text>
                 </View>
@@ -101,11 +110,11 @@ const Dashboard = () => {
                 width={4}
                 fill={75}
                 rotation={0}
-                tintColor="#06b6d4"
-                backgroundColor="#e2e8f0"
+                tintColor={colorScheme == "dark" ? "#00ffd1" : "#06b6d4"}
+                backgroundColor={colorScheme == "dark" ? "#2b1f4a" : "#e2e8f0"}
               >
                 {() => (
-                  <Text className="font-spaceBold text-[10px] text-[#020617] ">
+                  <Text className="font-spaceBold text-[10px] text-[#020617] dark:text-[#e6f0ff] ">
                     {75}
                   </Text>
                 )}
@@ -114,11 +123,11 @@ const Dashboard = () => {
             <View className="flex-row justify-between py-2.5 border-t border-[#e2e8f0cc]">
               <View className="flex-row gap-2 items-center">
                 <View className="w-2 h-2 bg-[#f49e0b] rounded-full"></View>
-                <Text className="text-base font-spaceRegular text-[#020617]">
+                <Text className="text-base font-spaceRegular text-[#020617] dark:text-white">
                   Due in 2h
                 </Text>
               </View>
-              <Text className="font-spaceRegular text-xs text-[#6b7280]">
+              <Text className="font-spaceRegular text-xs text-[#6b7280] dark:text-[#e6e9ff]">
                 Weekly
               </Text>
             </View>
