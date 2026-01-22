747ff01968448945a7da646ebcb885b29ec45ca6
machiloop <hamidusodiq2@gmail.com>
2026-01-22 14:35:52 +0100

    design dark mode for form picker input

--- components/formPickerInput.tsx
+++ components/formPickerInput.tsx
@@ -1,4 +1,5 @@
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useColorScheme } from "nativewind";
 import React, { useState } from "react";
 import { Control, Controller } from "react-hook-form";
 import { StyleSheet, Text, View } from "react-native";
@@ -25,11 +26,17 @@ const FormPickerInput = ({
   required,
 }: pickerData) => {
   const [isFocus, setIsFocus] = useState(false);
+  const { colorScheme } = useColorScheme();
+
+  const placeholderColor = colorScheme == "dark" ? "#616c82" : "#a2a9c6";
+  const selectedTextColor = colorScheme == "dark" ? "#e1ebfa" : "#0b1330";
+  const containerBackground = colorScheme == "dark" ? "#181c1f" : "#fff";
+  const itemTextColor = colorScheme == "dark" ? "white" : "black";
 
   return (
     <View>
       <View className="flex-row items-center gap-1.5">
-        <Text className="uppercase font-spaceSemibold text-[#8b93b8] text-sm">
+        <Text className="uppercase font-spaceSemibold text-[#8b93b8] dark:text-white text-sm">
           {label}
         </Text>
         {required && <Text className="text-red-500 font-spaceRegular">*</Text>}
@@ -42,14 +49,29 @@ const FormPickerInput = ({
           fieldState: { error },
         }) => (
           <>
-            <View className="flex  basis-auto px-3 py-1.5 mt-2 bg-white rounded-xl border border-[#ebebeb]">
+            <View className="flex  basis-auto px-3 py-1.5 mt-2 bg-white dark:bg-[#0e1626]  rounded-xl border dark:border-0 border-[#ebebeb]">
               <Dropdown
                 style={[styles.dropdown, isFocus && { borderColor: "#ebebeb" }]}
-                placeholderStyle={styles.placeholderStyle}
-                selectedTextStyle={styles.selectedTextStyle}
-                inputSearchStyle={styles.inputSearchStyle}
+                placeholderStyle={[
+                  styles.placeholderStyle,
+                  { color: placeholderColor },
+                ]}
+                selectedTextStyle={[
+                  styles.selectedTextStyle,
+                  { color: selectedTextColor },
+                ]}
+                inputSearchStyle={[
+                  styles.inputSearchStyle,
+                  { color: selectedTextColor },
+                ]}
                 iconStyle={styles.iconStyle}
                 data={data}
+                containerStyle={{
+                  backgroundColor: containerBackground,
+                  borderWidth: 0,
+                }}
+                activeColor={colorScheme == "dark" ? "#162032f2" : "#f4f4f4"}
+                itemTextStyle={{ color: itemTextColor }}
                 search
                 maxHeight={300}
                 labelField="label"
@@ -71,7 +93,7 @@ const FormPickerInput = ({
                     name="tag-outline"
                     size={20}
                     style={styles.icon}
-                    color="#8b93b8"
+                    color={colorScheme == "dark" ? "#99a6bf" : "#8b93b8"}
                   />
                 )}
               />
@@ -84,36 +106,6 @@ const FormPickerInput = ({
           </>
         )}
       />
-
-      {/* <Dropdown
-          style={[styles.dropdown, isFocus && { borderColor: "#ebebeb" }]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={data}
-          search
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          placeholder={!isFocus ? "Select Category" : "..."}
-          searchPlaceholder="Search..."
-          value={value}
-          onFocus={() => setIsFocus(true)}
-          onBlur={() => setIsFocus(false)}
-          onChange={(item) => {
-            setValue(item.value);
-            setIsFocus(false);
-          }}
-          renderLeftIcon={() => (
-            <MaterialCommunityIcons
-              name="tag-outline"
-              size={20}
-              style={styles.icon}
-              color="#8b93b8"
-            />
-          )}
-        /> */}
     </View>
   );
 };
@@ -145,12 +137,11 @@ const styles = StyleSheet.create({
   },
   placeholderStyle: {
     fontSize: 16,
-    color: "#0b1330",
     fontFamily: "SpaceGrotesk_400Regular",
   },
   selectedTextStyle: {
     fontSize: 16,
-    color: "#0b1330",
+    // color: "#0b1330",
     fontFamily: "SpaceGrotesk_400Regular",
   },
   iconStyle: {
