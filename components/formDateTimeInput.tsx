3e57b5dff375df90cd7bf146b3b2064772351290
machiloop <hamidusodiq2@gmail.com>
2026-01-22 14:47:44 +0100

    feat(ui): design dark mode for form date and time picker

--- components/formDateTimeInput.tsx
+++ components/formDateTimeInput.tsx
@@ -1,5 +1,6 @@
 import { getHoursAndMinutes } from "@/utils/formatTime";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useColorScheme } from "nativewind";
 import React, { useState } from "react";
 import { Control, Controller } from "react-hook-form";
 import { Pressable, Text, View } from "react-native";
@@ -15,6 +16,7 @@ type props = {
 
 const FormDateTimeInput = ({ label, name, control, mode, required }: props) => {
   const [open, setOpen] = useState(false);
+  const { colorScheme } = useColorScheme();
 
   return (
     <View className="flex-1">
@@ -28,7 +30,7 @@ const FormDateTimeInput = ({ label, name, control, mode, required }: props) => {
           <>
             <View>
               <View className="items-center flex-row gap-1.5">
-                <Text className="uppercase font-spaceSemibold text-[#8b93b8] text-sm">
+                <Text className="uppercase font-spaceSemibold text-[#8b93b8] dark:text-white text-sm">
                   {label}
                 </Text>
                 {required && (
@@ -37,22 +39,22 @@ const FormDateTimeInput = ({ label, name, control, mode, required }: props) => {
               </View>
               <Pressable
                 onPress={() => setOpen(true)}
-                className="flex-row gap-2 items-center border border-[#ebebeb] bg-white py-4  px-3 rounded-xl"
+                className="flex-row gap-2 items-center border border-[#ebebeb] bg-white dark:bg-[#0e1626] dark:border-0 py-4  px-3 rounded-xl"
               >
                 <MaterialCommunityIcons
                   name="calendar-blank-outline"
                   size={20}
-                  color="#8b93b8"
+                  color={colorScheme == "dark" ? "#99a6bf" : "#8b93b8"}
                 />
 
                 {value ? (
-                  <Text className="text-[#0b1330] font-spaceRegular">
+                  <Text className="text-[#0b1330] dark:text-[#e1ebfa] font-spaceRegular">
                     {mode == "date"
                       ? value.toLocaleDateString()
                       : getHoursAndMinutes(value)}
                   </Text>
                 ) : (
-                  <Text className="text-[#a2a9c6] font-spaceRegular">
+                  <Text className="text-[#a2a9c6] dark:text-[#616c82] font-spaceRegular">
                     {mode == "date" ? "Select Date" : "Select Time"}
                   </Text>
                 )}
